95d0511621da57c225774229ed5c359d6f28a990
Kristoffer Søholm <k.soeholm@gmail.com>
2024-03-28 21:49:33 +0100

    More commands

--- src/types.rs
+++ src/types.rs
@@ -71,6 +71,53 @@ pub struct InitHCAOutput {
     pub syndrome: u32,
 }
 
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "big", magic = b"\x01\x04\0\0\0\0\0\0\0\0\0\0\0\0\0\0")]
+pub struct EnableHCA(());
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "big")]
+pub struct EnableHCAOutput {
+    #[deku(pad_bytes_after = "3")]
+    pub status: u8,
+
+    #[deku(pad_bytes_after = "4")]
+    pub syndrome: u32,
+}
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "big", magic = b"\x01\x05\0\0\0\0\0\0\0\0\0\0\0\0\0\0")]
+pub struct DisableHCA(());
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "big")]
+pub struct DisableHCAOutput {
+    #[deku(pad_bytes_after = "3")]
+    pub status: u8,
+
+    #[deku(pad_bytes_after = "4")]
+    pub syndrome: u32,
+}
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "big", magic = b"\x01\x0a\0\0\0\0\0\0\0\0\0\0\0\0\0\0")]
+pub struct QueryISSI(());
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "big")]
+pub struct QueryISSIOutput {
+    #[deku(pad_bytes_after = "3")]
+    pub status: u8,
+
+    #[deku(pad_bytes_after = "2")]
+    pub syndrome: u32,
+
+    #[deku(pad_bytes_after = "20")]
+    pub current_issi: u16,
+
+    pub supported_issi: [u8; 0x50],
+}
+
 #[cfg(test)]
 mod tests {
     use std::io::Write;
@@ -158,5 +205,64 @@ mod tests {
 
         assert_eq!(res.len(), 0x10);
         assert_eq!(res, &[0x01, 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn test_start_hca() {
+        let cmd = EnableHCA(());
+
+        let res = cmd.to_bytes().unwrap();
+
+        assert_eq!(res.len(), 0x10);
+        assert_eq!(res, &[0x01, 0x04, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn test_disable_hca() {
+        let cmd = DisableHCA(());
+
+        let res = cmd.to_bytes().unwrap();
+
+        assert_eq!(res.len(), 0x10);
+        assert_eq!(res, &[0x01, 0x05, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn test_query_issi() {
+        let cmd = QueryISSI(());
+
+        let res = cmd.to_bytes().unwrap();
+
+        assert_eq!(res.len(), 0x10);
+        assert_eq!(res, &[0x01, 0x0a, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+        let out = QueryISSIOutput {
+            status: 0,
+            syndrome: 0,
+            current_issi: 0,
+            supported_issi: [0; 0x50],
+        };
+        println!("{}", out.to_bytes().unwrap().len());
+
+        #[rustfmt::skip]
+        let output: &[u8] = &[
+            0xab, 0x00, 0x00, 0x00, 0x12, 0x34, 0x56, 0x78, 0x00, 0x00, 0xaa, 0xbb, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+            0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
+            0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f,
+            0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f,
+            0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f,
+        ];
+
+        assert_eq!(
+            QueryISSIOutput::try_from(output).unwrap(),
+            QueryISSIOutput {
+                status: 0xab,
+                syndrome: 0x12345678,
+                current_issi: 0xaabb,
+                supported_issi: std::array::from_fn(|i| i as u8),
+            }
+        );
     }
 }
