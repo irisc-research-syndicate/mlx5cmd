4f0e504c79181fd90a2593f74112a0220d000094
Kristoffer Søholm <k.soeholm@gmail.com>
2024-03-28 20:26:33 +0100

    Add QueryPagesOutput

--- src/types.rs
+++ src/types.rs
@@ -17,6 +17,18 @@ pub enum QueryPagesOpMod {
 }
 
 #[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(endian = "big")]
+pub struct QueryPagesOutput {
+    #[deku(pad_bytes_after = "3")]
+    status: u8,
+
+    #[deku(pad_bytes_after = "4")]
+    syndrome: u32,
+
+    num_pages: u32,
+}
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
 #[deku(endian = "big", magic = b"\x01\x08")]
 pub struct ManagePages {
     #[deku(pad_bytes_before = "4", pad_bytes_after = "4")]
@@ -52,6 +64,19 @@ mod tests {
         assert_eq!(
             res,
             &[0x01, 0x07, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0]
+        );
+
+        let output: &[u8] = &[
+            0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x20, 0x22,
+        ];
+
+        assert_eq!(
+            QueryPagesOutput::try_from(output).unwrap(),
+            QueryPagesOutput {
+                status: 0,
+                syndrome: 0,
+                num_pages: 8226,
+            }
         );
     }
 
